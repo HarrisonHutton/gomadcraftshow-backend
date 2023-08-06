aa81e48f704a2a61d5aad1f919b69726440fc2f6
HarrisonHutton <harrison.hutton6@gmail.com>
2023-08-06 18:58:15 -0400

    ✨ Ask for confirmation before running the script

--- importspaces.py
+++ importspaces.py
@@ -9,16 +9,28 @@ Run this file with this command:
 python3 importspaces.py data/vendorspaces/2023_spaces.xlsx
 """
 
-# Replace with the path to your service account key JSON file
-cred = credentials.Certificate("environments/gomad-craft-show-firebase-adminsdk-uzbiz-133156689e.json")
-
-# Initialize the Firebase Admin SDK
-firebase_admin.initialize_app(cred)
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 importspaces.py <excel_file>")
         sys.exit(1)
+        
+    response = input("Are you sure you want to import spaces? This will delete all existing spaces. (y/n) ")
+    
+    if response != "y":
+        print("Aborting import.")
+        return
+        
+    # Replace with the path to your service account key JSON file
+    cred = credentials.Certificate("environments/gomad-craft-show-firebase-adminsdk-uzbiz-133156689e.json")
+
+    # Initialize the Firebase Admin SDK
+    firebase_admin.initialize_app(cred)
+    
+    # Delete the old firestore collection, 'vendorSpaces'
+    db = firestore.client()
+    collection_ref = db.collection("vendorSpaces")
+    delete_collection(collection_ref)
 
     spaces_excel_file = sys.argv[1]
     
@@ -37,9 +49,19 @@ def main():
 
     except Exception as e:
         print("An error occurred:", str(e))
+        
+    
+def delete_collection(coll_ref):
+    print("Deleting old spaces...")
+    
+    docs = coll_ref.list_documents()
+    
+    for doc in docs:
+        doc.delete()
 
 
 def excel_to_csv(excel_file):
+    print("Converting Excel file to CSV...")
     # Convert Excel file to a temporary CSV file
     df = pd.read_excel(excel_file)
     temp_csv_file = "temp.csv"
@@ -58,6 +80,8 @@ def excel_to_csv(excel_file):
 # }
 
 def process_data(csv_file) -> list[dict]:
+    print("Processing data...")
+    
     # Process the data from the CSV file (e.g., apply transformations, calculations)
     df = pd.read_csv(csv_file)
     # Do your data processing here
@@ -77,6 +101,8 @@ def process_data(csv_file) -> list[dict]:
 
 
 def write_to_firestore(data: list[dict]):
+    print("Writing data to Firestore...")
+    
     # Get a reference to the Firestore collection
     db = firestore.client()
     collection_ref = db.collection("vendorSpaces")
